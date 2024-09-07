04c66acc39a8e90235009105ce3b6c233788680b
Sebastian Schindler <sebastian@schindlertai.de>
2024-09-07 20:44:01 +0200

    Fixed Spelling Issue

--- template/src/pages/NotFound/index.tsx
+++ template/src/pages/NotFound/index.tsx
@@ -5,7 +5,7 @@ export default function NotFoundPage(): ReactElement {
   return (
     <div className={'container'}>
       <h1>Oops, what are you looking for here?</h1>
-      <h3>Unfortunately, I couldn't find what you were looking for. </h3>
+      <h3>Unfortunately, I couldn&apos;t find what you were looking for. </h3>
       <h3>
         To return to the homepage press <a href={'/'}>HERE</a>
       </h3>
